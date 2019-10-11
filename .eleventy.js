31b87c483dbb2e51c833ba41a8a1189e871bcfd4
Vadim Makeev <pepelsbey@gmail.com>
2019-10-11 19:54:07 +0300

    Добавляет несколько полезных фильтров (#49)

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,31 @@ module.exports = function(config) {
     config.addPassthroughCopy('src/fonts');
     config.addPassthroughCopy('src/styles');
 
+    config.addFilter('htmlmin', function(value) {
+        let htmlmin = require('html-minifier');
+        return htmlmin.minify(
+            value, {
+                removeComments: true,
+                collapseWhitespace: true
+            }
+        );
+    });
+
+    config.addFilter('markdown', function(value) {
+        let markdown = require('markdown-it')({
+            html: true
+        });
+        return markdown.render(value);
+    });
+
+    config.addTransform('xmlmin', function(content, outputPath) {
+        if(outputPath && outputPath.endsWith('.xml')) {
+            let prettydata = require('pretty-data');
+            return prettydata.pd.xmlmin(content);
+        }
+        return content;
+    });
+
     return {
         dir: {
             input: 'src',
