0dc45e06538fa8fa4a5127ba3b1ab28be3ec5a51
Vadim Makeev <pepelsbey@gmail.com>
2019-10-13 23:50:41 +0300

    Правит ошибки после пулреквеста

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -2,6 +2,6 @@ const navigationPages = document.querySelector('.navigation__pages');
 const navigationButton = document.querySelector('.navigation__button');
 
 navigationButton.addEventListener('click', event => {
-  event.target.classList.toggle('navigation__button--active');
-  navigationPages.classList.toggle('navigation__pages--active'); 
+    event.target.classList.toggle('navigation__button--active');
+    navigationPages.classList.toggle('navigation__pages--active');
 });
