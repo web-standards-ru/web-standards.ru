758903b36dfe30ab4ddf4f5ef197fdd88504dd12
EugZ <42672022+EugZ@users.noreply.github.com>
2019-10-26 02:11:10 +0300

    адаптивность хедера (#89)
    * убирает transform с активного состояния кнопки меню
    
    * делает лого более узким
    
    * перевёрстывает адаптацию навигации для декстопа
    
    * выравнивает ссылки в навигации на десктоп
    
    * добавляет атрибут состояния меню для кнопки меню
    
    * пытается прикрутить доступность для навигационного меню
    
    * исправляет баг с прозрачностью навигационного меню на мобильном устройстве
    
    * выставляет tabindex для поочерёдного переключения по ссылкам в навигации
    
    * Убирает лишние атрибуты
    
    * Сокращает запись гридов
    
    * Пишет стили для ссылок и сортирует свойства

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -1,7 +1,28 @@
 const navigationPages = document.querySelector('.navigation__pages');
 const navigationButton = document.querySelector('.navigation__button');
+let isMenuHidden = false;
 
-navigationButton.addEventListener('click', event => {
-    event.target.classList.toggle('navigation__button--active');
+const isMenuHiddenCheck = () => {
+  let attributeValue = navigationButton.getAttribute("aria-expanded");
+
+  attributeValue === 'false' ? isMenuHidden = false : isMenuHidden = true;
+
+  return isMenuHidden;
+}
+
+const handleNavigationMenuState = () => {
+    if (isMenuHiddenCheck()) {
+      navigationButton.setAttribute("aria-expanded", "false");
+      return;
+    } 
+    navigationButton.setAttribute("aria-expanded", "true");
+};
+
+const showHideNavigationMenu = () => {
     navigationPages.classList.toggle('navigation__pages--active');
+};
+
+navigationButton.addEventListener('click', () => {
+    handleNavigationMenuState();
+    showHideNavigationMenu();
 });
