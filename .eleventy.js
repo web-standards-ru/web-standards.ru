c464cff841e3c8c5aaecd815c6ceb8657d604a78
Ivan Psarew <psarew.ivan@yandex.ru>
2020-05-04 17:33:35 +0300

    Упрощает код, убирает цикл while

--- .eleventy.js
+++ .eleventy.js
@@ -163,26 +163,17 @@ module.exports = function(config) {
     config.addTransform('videoInsert', (content, outputPath) => {
         let reg = /articles\/([a-zA-Z0-9_-]+)\/index\.html/i;
         if (outputPath && outputPath.match(reg)) {
-            let regId = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i;
-            let target = '<iframe src="https://www.youtube.com';
-            let position = -1;
-            while ((position = content.indexOf(target, position + 1)) != -1) {
-                let startFrame = content.indexOf(target, 0);
-                let endFrame = content.indexOf('</iframe', startFrame) + 9;
-                let videoId = content.slice(startFrame, endFrame)
-                    .match(regId)[1];
-                content = content.replace(
-                    /\<iframe src\=\"https\:\/\/www\.youtube\.com\/embed\/([a-zA-Z0-9_-]+)\"(.*?)\>\<\/iframe>/,
-                    `<div class="video">
-                        <a class="video__link" href="https://youtu.be/${videoId}">
+            content = content.replace(/\<iframe src\=\"https\:\/\/www\.youtube\.com\/embed\/([a-zA-Z0-9_-]+)\"(.*?)\>\<\/iframe>/ig, (match, p1) => {
+                return `
+                    <div class="video">
+                        <a class="video__link" href="https://youtu.be/${p1}">
                             <img class="video__media" src="images/hqdefault.jpg" alt="">
                         </a>
                         <button class="video__button" type="button" aria-label="Запустить видео">
                             <svg width="68" height="48" viewBox="0 0 68 48"><path class="video__button-shape" d="M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z"></path><path class="video__button-icon" d="M 45,24 27,14 27,34"></path></svg>
                         </button>
                     </div>`
-                );
-            }
+            });
         }
         return content;
     });
