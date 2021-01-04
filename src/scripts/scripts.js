9cf0bf944e84ae3d45ef7f4d12dc65b420762ff3
Ivan Psarew <psarew.ivan@yandex.ru>
2021-01-04 15:16:51 +0300

    Добавляет описание состояния кнопки меню в шапке

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -7,6 +7,7 @@
     menuButton.addEventListener('click', () => {
         let expanded = menuButton.getAttribute('aria-expanded') === 'true';
         menuButton.setAttribute('aria-expanded', !expanded);
+        expanded ? menuButton.setAttribute('aria-label', 'Открыть меню') : menuButton.setAttribute('aria-label', 'Закрыть меню');
         menuButton.classList.toggle('menu__button--open');
         menuList.classList.toggle('menu__list--open');
     });
