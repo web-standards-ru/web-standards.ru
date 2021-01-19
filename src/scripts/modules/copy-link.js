24006e8a630d78ed9ecb400db63a88c3cc675496
Vadim Makeev <pepelsbey@gmail.com>
2021-01-19 13:58:17 +0300

    Радует линтер

--- src/scripts/modules/copy-link.js
+++ src/scripts/modules/copy-link.js
@@ -1,61 +1,61 @@
 const buttons = [...document.querySelectorAll('.tooltip__button')];
-const successClassName = 'tooltip__label--success'
-const errorClassName = 'tooltip__label--error'
-let timeout
+const successClassName = 'tooltip__label--success';
+const errorClassName = 'tooltip__label--error';
+let timeout;
 
 function setDefaultState(tooltip) {
-    tooltip.innerText = 'Скопировать ссылку'
-    tooltip.classList.remove(successClassName)
+    tooltip.innerText = 'Скопировать ссылку';
+    tooltip.classList.remove(successClassName);
 }
 
 function setSuccessState(tooltip) {
-    tooltip.innerText = 'Скопировано'
-    tooltip.classList.add(successClassName)
+    tooltip.innerText = 'Скопировано';
+    tooltip.classList.add(successClassName);
 }
 
 function setErrorState(tooltip) {
-    tooltip.innerText = 'Не удалось скопировать'
-    tooltip.classList.remove(errorClassName)
+    tooltip.innerText = 'Не удалось скопировать';
+    tooltip.classList.remove(errorClassName);
 }
 
 function handleCopy() {
-    const tooltip = this.nextSibling
-    const hash = this.getAttribute('data-href')
+    const tooltip = this.nextSibling;
+    const hash = this.getAttribute('data-href');
 
     navigator.clipboard.writeText(`${window.location.href}${hash}`)
         .then(() => {
-            setSuccessState(tooltip)
+            setSuccessState(tooltip);
         })
         .catch(() => {
-            setErrorState(tooltip)
-        })
+            setErrorState(tooltip);
+        });
 }
 
 function handleMouseEnter() {
-    const tooltip = this.nextSibling
+    const tooltip = this.nextSibling;
 
     if (timeout) {
-        return
+        return;
     }
 
-    setDefaultState(tooltip)
+    setDefaultState(tooltip);
 }
 
 function handleMouseOut() {
-    const tooltip = this.nextSibling
+    const tooltip = this.nextSibling;
 
     if (!tooltip.classList.contains(successClassName) && !tooltip.classList.contains(errorClassName)) {
-        return
+        return;
     }
 
     timeout = setTimeout(() => {
-        this.blur()
-        setDefaultState(tooltip)
-    }, 1000)
+        this.blur();
+        setDefaultState(tooltip);
+    }, 1000);
 }
 
 buttons.forEach((button) => {
-    button.addEventListener('click', handleCopy)
-    button.addEventListener('mouseenter', handleMouseEnter)
-    button.addEventListener('mouseout', handleMouseOut)
-})
+    button.addEventListener('click', handleCopy);
+    button.addEventListener('mouseenter', handleMouseEnter);
+    button.addEventListener('mouseout', handleMouseOut);
+});
