22f39e368803b0571ea17dfa95252ba73b85fb1b
Pavel Mineev <pavel@mineev.me>
2021-01-19 15:44:57 +0300

    Копирование ссылки раздела статьи (#141)
    * #82: Копирование ссылки раздела статьи
    
    * Чиним линтеры
    
    * Верный цвет для тултипа с ошибкой
    
    * Всё ломает
    
    * Убирает лишний defs
    
    * почил сломанное, причесал JS
    
    * Делает тултипы аккуратнее
    
    * Правит ошибку мёржа
    
    * Радует линтер
    
    * Удаляет дубликат и сортирует
    
    * Правит выравнивание в Firefox
    
    Co-authored-by: Vadim Makeev <pepelsbey@gmail.com>
    Co-authored-by: mefody <n.a.dubko@gmail.com>

---
+++ src/scripts/modules/copy-link.js
@@ -0,0 +1,61 @@
+const buttons = [...document.querySelectorAll('.tooltip__button')];
+const successClassName = 'tooltip__label--success';
+const errorClassName = 'tooltip__label--error';
+let timeout;
+
+function setDefaultState(tooltip) {
+    tooltip.innerText = 'Скопировать ссылку';
+    tooltip.classList.remove(successClassName);
+}
+
+function setSuccessState(tooltip) {
+    tooltip.innerText = 'Скопировано';
+    tooltip.classList.add(successClassName);
+}
+
+function setErrorState(tooltip) {
+    tooltip.innerText = 'Не удалось скопировать';
+    tooltip.classList.remove(errorClassName);
+}
+
+function handleCopy() {
+    const tooltip = this.nextSibling;
+    const hash = this.getAttribute('data-href');
+
+    navigator.clipboard.writeText(`${window.location.href}${hash}`)
+        .then(() => {
+            setSuccessState(tooltip);
+        })
+        .catch(() => {
+            setErrorState(tooltip);
+        });
+}
+
+function handleMouseEnter() {
+    const tooltip = this.nextSibling;
+
+    if (timeout) {
+        return;
+    }
+
+    setDefaultState(tooltip);
+}
+
+function handleMouseOut() {
+    const tooltip = this.nextSibling;
+
+    if (!tooltip.classList.contains(successClassName) && !tooltip.classList.contains(errorClassName)) {
+        return;
+    }
+
+    timeout = setTimeout(() => {
+        this.blur();
+        setDefaultState(tooltip);
+    }, 1000);
+}
+
+buttons.forEach((button) => {
+    button.addEventListener('click', handleCopy);
+    button.addEventListener('mouseenter', handleMouseEnter);
+    button.addEventListener('mouseout', handleMouseOut);
+});
