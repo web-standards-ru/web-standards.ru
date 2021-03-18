4b6a7c0404a7a4bcd62ff6296a73435267411251
monochromer <monochromer@users.noreply.github.com>
2021-03-18 22:29:58 +0500

    Использование `eleventyComputed` для получения данных о людях (#216)

---
+++ src/articles/articles.11tydata.js
@@ -0,0 +1,28 @@
+function filterPeople(peopleList, filterList) {
+    return peopleList
+        .filter((person) => {
+            // Иногда filterList это строка, а не массив, поэтому всегда делаем массив
+            const filterListNormalized = [].concat(filterList);
+
+            return filterListNormalized.includes(person.fileSlug);
+        })
+        .map((person) => {
+            return person.data;
+        });
+}
+
+module.exports = {
+    eleventyComputed: {
+        authorData: function(data) {
+            return filterPeople(data.collections.people, data.author)
+        },
+
+        translatorsData: function(data) {
+            return filterPeople(data.collections.people, data.translators)
+        },
+
+        editorsData: function(data) {
+            return filterPeople(data.collections.people, data.editors)
+        }
+    }
+}
