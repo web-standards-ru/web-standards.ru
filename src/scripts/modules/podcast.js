72e823843aec1025c031fbb1040259383243c140
Vadim Makeev <pepelsbey@gmail.com>
2021-04-17 11:54:33 +0300

    Перевёрстывает таймкоды

--- src/scripts/modules/podcast.js
+++ src/scripts/modules/podcast.js
@@ -1,9 +1,9 @@
 const player = document.querySelector('.podcast__player');
-const timeline = document.querySelector('.podcast__timeline');
+const timecode = document.querySelector('.podcast__timecode');
 
-if (timeline && player) {
-    timeline.addEventListener('click', event => {
-        const button = event.target.closest('.podcast__timeline-button');
+if (timecode && player) {
+    timecode.addEventListener('click', event => {
+        const button = event.target.closest('.podcast__timecode-button');
 
         if (!button) {
             return;
