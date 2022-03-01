239361fef7b9e66c4ed44b4a8864a8957bead64e
Ivan Uhalin <vanyauhalin@gmail.com>
2022-03-01 18:14:16 +0300

    Добавляет обработку timecode из url (#300)
    * Добавляет обработку timecode из url
    
    * Добавляет обработку короткого timecode
    
    * Добавляет обновление якоря
    
    * Добавляет слушатель для якоря
    
    * Добавляет комментарии, правит названия
    
    * Разворачивает str в string
    
    Co-authored-by: Vadim Makeev <pepelsbey@gmail.com>

--- src/scripts/modules/podcast.js
+++ src/scripts/modules/podcast.js
@@ -1,18 +1,77 @@
 const player = document.querySelector('.podcast__player');
 const timecode = document.querySelector('.podcast__timecode');
 
-if (timecode && player) {
-    timecode.addEventListener('click', event => {
+/**
+ * Выделяет из строки временные метки вида `00:14:30` или `14:30`.
+ *
+ * @param {string} string
+ * @returns {string | null}
+ */
+function matchTimecode(string) {
+    const match = string.match(/.+#(\d\d:\d\d(:\d\d)?)$/);
+    return match && match[1] ? match[1] : null;
+}
+
+/**
+ * Преобразует временные метки вида `00:14:30` или `14:30` в количество секунд.
+ *
+ * @param {string} time
+ * @returns {number}
+ */
+function parseTimecode(time) {
+    return time.split(':').reduceRight((acc, item, index, items) => {
+        return acc += parseFloat(item) * Math.pow(60, items.length - 1 - index);
+    }, 0);
+}
+
+/**
+ * @param {number} time
+ * @returns {void}
+ */
+function setTime(time) {
+    player.currentTime = time;
+}
+
+function handlePassedTimecode() {
+    const passed = matchTimecode(document.URL);
+
+    if (passed) {
+        setTime(parseTimecode(passed));
+    }
+}
+
+function initAnchor() {
+    window.addEventListener('hashchange', handlePassedTimecode);
+}
+
+/**
+ * @param {string} time
+ * @returns {void}
+ */
+function setAnchor(time) {
+    window.location.hash = `#${time}`;
+}
+
+function playAudio() {
+    if (player.paused) {
+        player.play();
+    }
+}
+
+function initTimecode() {
+    timecode.addEventListener('click', (event) => {
         const button = event.target.closest('.podcast__timecode-button');
 
-        if (!button) {
-            return;
-        }
-
-        const time = parseFloat(button.value);
-        player.currentTime = time;
-        if (player.paused) {
-            player.play();
+        if (button) {
+            setTime(parseFloat(button.value));
+            setAnchor(button.innerText);
+            playAudio();
         }
     });
+}
+
+if (timecode && player) {
+    handlePassedTimecode();
+    initAnchor();
+    initTimecode();
 }
