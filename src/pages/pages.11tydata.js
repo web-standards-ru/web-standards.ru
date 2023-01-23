c787e2f1910fc57d2520bde818e89adf1e28e0e5
Vadim Makeev <hi@pepelsbey.dev>
2023-01-23 03:44:41 +0100

    Упрощает превью, реверсит вывод

--- src/pages/pages.11tydata.js
+++ src/pages/pages.11tydata.js
@@ -2,22 +2,6 @@ module.exports = {
     numberFeaturedEpisodes: 5,
 
     eleventyComputed: {
-        filteredPodcasts: function(data) {
-            const { episodes } = data.collections;
-
-            if (!episodes) {
-                return null;
-            }
-
-            const featuredEpisodes = episodes.slice(0, data.numberFeaturedEpisodes);
-            const restEpisodes = episodes.slice(data.numberFeaturedEpisodes);
-
-            return {
-                featuredEpisodes,
-                restEpisodes,
-            };
-        },
-
         indexArticles: function(data) {
             const articles = data.collections.articles.slice().reverse();
 
