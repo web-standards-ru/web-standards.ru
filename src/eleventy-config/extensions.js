893510af6806779f3c7cf521ede4572407e850d5
monochromer <monochromer@users.noreply.github.com>
2024-08-08 16:09:53 +0500

    Удаляет gulp из сборки (#369)
    * Удаляет gulp из сборки
    
    * Явно задаёт кодировку `utf-8` для esbuild

--- src/eleventy-config/extensions.js
+++ src/eleventy-config/extensions.js
@@ -1,7 +1,32 @@
 import yaml from 'js-yaml';
+import eleventyPluginCss from '../libs/eleventy-plugin-css.js';
+import eleventyPluginJs from '../libs/eleventy-plugin-js.js';
+
+const isProdMode = process.env.NODE_ENV === 'production';
 
 export default async function(eleventyConfig) {
     eleventyConfig.addDataExtension('yml', (contents) => {
         return yaml.load(contents);
+    });
+
+    eleventyConfig.ignores.add('src/**/!(styles)/*.css');
+    eleventyConfig.ignores.add('src/styles/fonts.css');
+
+    eleventyConfig.addPlugin(eleventyPluginCss, {
+        /** @type (import('lightningcss').BundleAsyncOptions */
+        lightningcss: {
+            minify: isProdMode,
+        },
+    });
+
+    eleventyConfig.ignores.add('src/**/!(scripts)/*.js');
+    eleventyConfig.ignores.add('src/scripts/modules/**/*.js');
+
+    eleventyConfig.addPlugin(eleventyPluginJs, {
+        /** @type (import('esbuild').BuildOptions */
+        esbuild: {
+            platform: 'browser',
+            minify: isProdMode,
+        },
     });
 }
