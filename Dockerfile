598a0dc7842a61df32096e456a498db651f7195e
Polina Gurtovaya <zloymult@gmail.com>
2025-05-28 00:30:01 +0100

    Update docker file, fix permissions and fetch nginx conf

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,27 @@
-FROM node:lts-alpine AS builder
+# syntax=docker/dockerfile:1
 
-# Install deps
+FROM node:lts-alpine AS builder
 WORKDIR /app
 COPY . .
 RUN apk add --no-cache git
 RUN npm install
 RUN npm run build
 
+FROM alpine AS nginx-config
+WORKDIR /nginx
+RUN apk add --no-cache git openssh openssh-client
+RUN mkdir -p -m 700 /root/.ssh && ssh-keyscan github.com >> /root/.ssh/known_hosts
+RUN --mount=type=ssh git clone --depth 1 git@github.com:web-standards-ru/nginx.git .
+RUN rm -rf .git
 
-FROM nginx:stable-alpine AS prod
-
-ARG NGINX_CONF=nginx.conf
+FROM nginx:stable
 ARG NGINX_LOCAL=nginx.local.conf
+ARG NGINX_CONF=nginx.conf
 
+COPY --from=nginx-config /nginx/ /etc/nginx/
 COPY --from=builder /app/dist /var/www/web-standards.ru/html
 COPY --from=builder /app/${NGINX_CONF} /etc/nginx/conf.d/default.conf
 COPY --from=builder /app/${NGINX_LOCAL} /etc/nginx/${NGINX_LOCAL}
 
 EXPOSE 80
-CMD ["nginx","-g","daemon off;"]
+CMD ["nginx", "-g", "daemon off;"]
--- docker.sh
+++ docker.sh
@@ -1,30 +1,38 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+export DOCKER_BUILDKIT=1
+
+
+if [ -z "$SSH_AUTH_SOCK" ]; then
+  echo "❌ SSH agent not available. Run 'ssh-agent' and 'ssh-add' first."
+  exit 1
+fi
+
+
 task="${1:-}"
 certs_dir="${2:-./certs}"
 
 case "$task" in
-  build)
-    docker build -t webstandards_ru --build-arg 'NGINX_CONF=nginx.conf' --build-arg 'NGINX_LOCAL_CONF=nginx.local.conf' .
-    ;;
-  run-local)
-    docker run --rm -p 8080:80 webstandards_ru nginx -g 'daemon off;' -c /etc/nginx/nginx.local.conf
-    ;;
-  run-prod)
-    [ -n "$certs_dir" ] || { printf "Нет сертификатов. Проверьте папку $certs_dir\n" >&2; exit 1; }
-    docker run --rm \
-      -v "$certs_dir":/etc/letsencrypt/live/web-standards.ru:ro \
-      -p 80:80 \
-      -p 443:443 \
-      -e NGINX_CONF=nginx.conf \
-      webstandards_ru
-    ;;
-  debug)
-    docker run --rm -it -p 8080:80 webstandards_ru sh
-    ;;
-  *)
-    printf "Неправильная команда, должно быть что-то из этого: %s {build|run-local|run-prod}\n" "$(basename "$0")" >&2
-    exit 1
-    ;;
+    build)
+        docker buildx build --ssh default -t webstandards_ru --build-arg 'NGINX_CONF=nginx.conf' --build-arg 'NGINX_LOCAL_CONF=nginx.local.conf' .
+        ;;
+    run-local)
+        docker run --rm -p 8080:80 webstandards_ru nginx -g 'daemon off;' -c /etc/nginx/nginx.local.conf
+        ;;
+    run-prod)
+        [ -n "$certs_dir" ] || { printf "Нет сертификатов. Проверьте папку $certs_dir\n" >&2; exit 1; }
+        docker run --rm \
+        -v "$certs_dir":/etc/letsencrypt/live/web-standards.ru:ro \
+        -p 80:80 \
+        -p 443:443 \
+        webstandards_ru
+        ;;
+    debug)
+        docker run --rm -it -p 8080:80 webstandards_ru sh
+        ;;
+    *)
+        printf "Неправильная команда, должно быть что-то из этого: %s {build|run-local|run-prod}\n" "$(basename "$0")" >&2
+        exit 1
+        ;;
 esac
