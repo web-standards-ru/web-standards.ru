f0e5389576c721c7eb3602186495e86056011d7c
monochromer <monochromer@users.noreply.github.com>
2025-08-18 15:09:51 +0500

    Упрощает вычисление размера скроллбара (#394)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,4 +2,3 @@ import './modules/menu.js';
 import './modules/copy-link.js';
 import './modules/video.js';
 import './modules/podcast.js';
-import './modules/scrollbar-size.js';
--- src/scripts/modules/scrollbar-size.js
+++
@@ -1,9 +0,0 @@
-function setScrollbarSize() {
-    const root = document.documentElement;
-    root.style.setProperty('--scrollbar-size', window.innerWidth - root.offsetWidth);
-}
-
-setScrollbarSize();
-
-window.addEventListener('resize', setScrollbarSize);
-window.screen.orientation.addEventListener('change', setScrollbarSize);
